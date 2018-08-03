47f667a7669177fdacefd66124af8bd67a678c43
Veronica Bustabad <vbustabad@gmail.com>
2018-08-03 14:35:30 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	
 	def index
 		@posts = Post.all
 	end
@@ -10,4 +11,13 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def create
+	  @post = Post.new
+	  @post.title = params[:title]
+	  @post.description = params[:description]
+	  @post.save
+	  redirect_to post_path(@post)
+	end
+	
 end
